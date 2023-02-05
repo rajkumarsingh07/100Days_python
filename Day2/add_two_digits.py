7fd44d64c2b931d4f43b0e49c392929bc5968618
rajkumarsingh07 <rajkumarsingh07@gmail.com>
2023-02-06 00:02:08 +0530

    modified

--- Day2/add_two_digits.py
+++ Day2/add_two_digits.py
@@ -1,10 +1,10 @@
+#Enter any two digit number and need output of after added them each number like 35 the 3+5=8
 two_digit_number = input("type two digit number: ")
 print(type(two_digit_number))
 first_digit = two_digit_number[0]
 second_digit = two_digit_number[1]
 
-print(type(first_digit))
 print(first_digit)
 print(second_digit)
-result = int(first_digit) + int(second_digit )
+result = int(first_digit) + int(second_digit ) #convert from str to intger to get each number addition
 print(result)
