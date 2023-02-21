5300c13ed738843c59718280edde7d7636319160
rajkumarsingh07 <rajkumarsingh07@gmail.com>
2023-02-21 20:52:17 +0530

    updated

--- Day5/passwd_generator_5.7.py
+++ Day5/passwd_generator_5.7.py
@@ -11,37 +11,43 @@ nr_symbols = int(input("How many symbols would you like in your passwd?\n"))
 nr_numbers = int(input("How many numbers would you like in your passwd?\n"))
 
 #Easy_level:- 
-passwd = " "
-# range to 1 to 4
-for char in range(1, nr_letters + 1): 
-#  randon_char = random.choice(letters)
-#  passwd = passwd + randon_char
-   passwd += random.choice(letters)
+# passwd = " "
+# # range to 1 to 4
+# for char in range(1, nr_letters + 1): 
+# #  randon_char = random.choice(letters)
+# #  passwd = passwd + randon_char
+#    passwd += random.choice(letters)
 
-for char in range(1, nr_symbols + 1): 
-    passwd += random.choice(symbols)
+# for char in range(1, nr_symbols + 1): 
+#     passwd += random.choice(symbols)
 
-for char in range(1, nr_numbers + 1): 
-#    passwd += int(random.choice(numbers))
-    randon_char = random.choice(numbers)
-    passwd = passwd + str(randon_char)
-print(f" Our final random passwd is: {passwd}")
+# for char in range(1, nr_numbers + 1): 
+# #    passwd += int(random.choice(numbers))
+#     randon_char = random.choice(numbers)
+#     passwd = passwd + str(randon_char)
+# print(f" Our final random passwd is: {passwd}")
 
 #Hard_level:- 
 
-# passwd_list = []
-# for char in range(1, nr_letters + 1): 
-#    passwd_list += random.choice(letters)
-# for char in range(1, nr_symbols + 1): 
-#     passwd_list += random.choice(symbols)
-# for char in range(1, nr_numbers + 1): 
-#     randon_char = random.choice(numbers)
-#     passwd_list = str(passwd_list) + str(randon_char)
-# print(passwd_list)
+passwd_list = []
+for char in range(1, nr_letters + 1): 
+   passwd_list += random.choice(letters)
+#print(passwd_list)
+for char in range(1, nr_symbols + 1): 
+    passwd_list += random.choice(symbols)
+#print(passwd_list)
+for char in range(1, nr_numbers + 1): 
+    passwd_list += str(random.choice(numbers))
+#print(passwd_list)
 
-# #ramdom.shuffle()
+ #ramdom.shuffle() We can exaplore on the url https://stackoverflow.com/questions/2177590/how-can-i-reorder-a-list //Ans 15
 
-# random.shuffle(passwd_list)
-# print(passwd_list)
+random.shuffle(passwd_list)
+#print(passwd_list)
+
+passwd = " "
+for char in passwd_list:
+    passwd += char
+print(f"Your Final Passwd is: {passwd}")
 
 
